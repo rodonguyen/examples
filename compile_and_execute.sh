714c395facb8f8b6c3d347b8639abba978e6af0c
Rodo <rodonguyendd@gmail.com>
2023-02-02 16:40:58 +1000

    train_only call graph

--- compile_and_execute.sh
+++ compile_and_execute.sh
@@ -55,7 +55,7 @@ file06='mnist_cnn_1convo_6'
 file='mnist_cnn_ds1000_1convo'
 output=${file}
 # g++ ${file}.cpp -o ${output} -larmadillo
-./${output}
+# ./${output}
 # ./${output} > console_output_01_convo.txt
 
 
@@ -63,7 +63,7 @@ output=${file}
 # valgrind --tool=callgrind ./${output}
 
 # GENERATE CALL GRAPH
-# gprof2dot ./cg_ds1000_1convo.out.32608 -f callgrind -n 0.1 -e 0.02 -s | dot -Tpng -o ./cg_ds1000_1convo.png
+gprof2dot ./callgrind.out.61749 -f callgrind -n 0.1 -e 0.02 -s | dot -Tpng -o ./mnist_cnn_train_only_02.png
 
 
 # htop --delay=1 --pid=
